488c86e3c062d1dbbd43276aa54dede1094c9ce0
Hafiz Raza Mohy ud Din <hdin01@i2c.com>
2026-06-22 20:59:52 +0500

    Update to Next.js 15.5.18

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1b07ccfca081d716")
+/******/ 		__webpack_require__.h = () => ("6227149c8001e0e3")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
