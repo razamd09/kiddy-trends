ce4d59114fcab8ba5a65667bb9260401057aff41
hdin01 <hdin01@i2cinc.com>
2026-06-25 15:08:56 +0500

    Remove Tawk.to live chat plugin

--- app/layout.js
+++ app/layout.js
@@ -73,21 +73,6 @@ export default function RootLayout({ children }) {
       <Script src="https://www.tiktok.com/embed.js" strategy="lazyOnload" />
       <Script src="https://www.instagram.com/embed.js" strategy="lazyOnload" />
 
-      {/* Tawk.to Live Chat */}
-      <Script id="tawk-to" strategy="lazyOnload">
-        {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6a3c0c849358261d45a3012b/1jrt91qcr';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-      </Script>
-
       <CartProvider>
         <RewardsPopup />
         <LiveNotifications />
