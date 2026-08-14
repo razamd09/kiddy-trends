4f028ac9a8cc1e88ea781ba304e222d3c8ea2b35
hdin01 <hdin01@i2cinc.com>
2026-08-14 14:05:19 +0500

    Full-width landscape promo banner

--- components/HomeHeroSlider.js
+++ components/HomeHeroSlider.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 export default function HomeHeroSlider() {
   return (
     <section className="w-full py-2 sm:py-3">
-      <div className="mx-auto max-w-7xl px-3 sm:px-6 lg:px-8">
-        <div className="relative overflow-hidden rounded-[1.5rem] bg-[#dfe9e2] ring-1 ring-black/5">
+      <div className="mx-auto w-full px-0 sm:px-0 lg:px-0">
+        <div className="relative overflow-hidden bg-[#dfe9e2]">
           <Image
             src="/sale-1947.png"
             alt="Kiddy Trends sale banner"
-            width={1600}
+            width={1800}
             height={700}
             priority
-            className="block h-auto max-h-[300px] w-full object-contain sm:max-h-[340px] lg:max-h-[360px]"
+            className="block h-auto w-full object-contain"
           />
         </div>
       </div>
