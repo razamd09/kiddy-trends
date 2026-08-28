339f6cc0694ac3bcb52f7009bbd0f11391027615
hdin01 <hdin01@i2cinc.com>
2026-08-28 18:17:23 +0500

    Remove free shipping loyalty section

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import RewardsChecker from '../components/RewardsChecker'
 import DiscountBanner from '../components/DiscountBanner'
 import LandingPreferencePopup from '../components/LandingPreferencePopup'
 import HomeHeroSlider from '../components/HomeHeroSlider'
-import FreeShippingLoyaltyBanner from '../components/FreeShippingLoyaltyBanner'
 import LazyMount from '../components/LazyMount'
 import { compareNewestNewArrivalsFirst, isNewArrivalsVersion } from '../lib/newArrivals'
 
@@ -101,7 +100,6 @@ export default function Home() {
         <DiscountBanner />
         {/* HERO */}
         <HomeHeroSlider />
-        <FreeShippingLoyaltyBanner />
 
         {/* SHOP BY COLLECTION */}
         <section className="w-full pt-12">
